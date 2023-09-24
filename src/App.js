b53bd1240cb4dfaf831331a13233e5489afe2c03
Johan Nyman <johan.nyman97@gmail.com>
2023-09-25 00:22:40 +0200

    la till routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import { Header } from "./components/header";
+import { ExplorePage } from "./pages/explorePage";
+import { HomePage } from "./pages/homePage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-	return <div className="App">test</div>;
+	return (
+		<Router>
+			<div className="flex-column justify-center App">
+				<Header />
+				<div className="flex justify-center">
+					<Routes>
+						<Route path="/explore" element={<ExplorePage />} />
+						<Route exact path="/" element={<HomePage />} />
+					</Routes>
+				</div>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
